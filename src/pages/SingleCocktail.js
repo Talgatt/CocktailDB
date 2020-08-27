f28b2f436c9d92abd59f8172d736432a099d0b10
Talgatt <talgat001@gmail.com>
2020-08-27 11:38:36 -0400

    initial commit

---
+++ src/pages/SingleCocktail.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+export default function SingleCocktail() {
+  console.log(useParams());
+  const { id } = useParams();
+
+  return <h1>single cocktail page: {id}</h1>;
+}
